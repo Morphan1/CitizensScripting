3615e0e0688ba85eadc3d499bae45ac408335263
Morphan1 <morphanone@gmail.com>
2016-09-14 03:14:06 -0400

    Update package and README.md

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ CitizensScripting
 =================
 
 Accesses the CitizensAPI's built-in scripting code to allow dynamic runtime JavaScripting.
+
+###Usage
+
+1. Download and install the latest Citizens2
+2. Download the latest CitizensScripting build from https://morphanone.com/plugins/CitizensScripting.jar
+3. Start and stop your server completely
+4. In `/plugins/CitizensScripting`, create a JavaScript file. It can be named anything and you can have as many files as you want.
+5. Make your script! Here's a nice example of how http://one.denizenscript.com/haste/36038
+6. You can run `/csreload` in-game to reload scripts on the fly!
--- src/main/java/com/morphanone/citizensscripting/CitizensScripting.java
+++ src/main/java/com/morphanone/citizensscripting/CitizensScripting.java
@@ -1,10 +1,13 @@
-package net.Morphan1.citizensscripting;
+package com.morphanone.citizensscripting;
 
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.scripting.CompileCallback;
 import net.citizensnpcs.api.scripting.Script;
+import net.citizensnpcs.api.scripting.ScriptCompiler;
 import net.citizensnpcs.api.scripting.ScriptFactory;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -26,8 +29,13 @@ public class CitizensScripting extends JavaPlugin {
     public void reload() {
         getServer().getLogger().info("[CitizensScripts] Reloading scripts.");
         clearScripts();
-        for (File file : getDataFolder().listFiles(jsFileFilter)) {
-            scripts.add(CitizensAPI.getScriptCompiler().compile(file).withCallback(new BehaviourCallback()).beginWithFuture());
+        File[] files = getDataFolder().listFiles(jsFileFilter);
+        if (files == null) {
+            return;
+        }
+        ScriptCompiler compiler = CitizensAPI.getScriptCompiler();
+        for (File file : files) {
+            scripts.add(compiler.compile(file).withCallback(new BehaviourCallback()).beginWithFuture());
         }
     }
 
@@ -52,6 +60,15 @@ public class CitizensScripting extends JavaPlugin {
                 }
             });
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equals("csreload")) {
+            reload();
+            return true;
+        }
+        return false;
     }
 
     private static final FilenameFilter jsFileFilter = new FilenameFilter() {
